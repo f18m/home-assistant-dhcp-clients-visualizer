fd348c5b6b02c291d1db9ec0f493b7550b81a52d
f18m <francesco.montorsi@gmail.com>
2024-09-10 00:12:26 +0200

    Improve output

---
+++ README.md
@@ -0,0 +1,8 @@
+# Home Assistant DHCP clients Visualizer
+
+This project provides a Python utility to pretty print DHCP clients 
+currently connected to your DHCP server.
+
+By default it assumes the DHCP server is running as Home Assistant addon: 
+https://github.com/home-assistant/addons/blob/master/dhcp_server/README.md
+
--- visualizer/printer.py
+++ visualizer/printer.py
@@ -1,10 +1,11 @@
 from dhcp_leases import Lease, DhcpLeases
 
 leases = DhcpLeases('/usr/share/hassio/addons/data/core_dhcp_server/dhcpd.lease')
-leases.get()  # Returns the leases as a list of Lease objects
-leases.get_current()  # Returns only the currently valid dhcp leases as dict
+curr = leases.get_current()  # Returns only the currently valid dhcp leases as dict
                       # The key of the dict is the device mac address and the
                       # Value is a Lease object
 
-for mac in leases.get_current():
-    print(mac)
+print("Current DHCP clients:")
+for mac in curr:
+    x = curr[mac]
+    print(f"{x.ip} ({x.mac}), hostname={x.hostname}")
